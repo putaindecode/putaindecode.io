d1dc5b901ef68929394035ac5abaa892debc0276
Maxime Thirouin <m@moox.io>
2013-08-15 08:11:20 +0200

    Add konami code
    Ref #7

---
+++ src/assets/_scripts/konami.js
@@ -0,0 +1,41 @@
+if (window.addEventListener) {
+  var kkeys = [],
+    //        ↑  ↑  ↓  ↓  ←  →  ←  →  b  a
+    konami = "38,38,40,40,37,39,37,39,66,65";
+    konami_cancel = "27"; // esc
+    el_konami = null;
+  window.addEventListener("keydown", function(e) {
+    kkeys.push(e.keyCode); 
+    if (kkeys.toString().indexOf(konami) >= 0) {
+      // reset
+      kkeys = [];
+      
+      el_konami = document.createElement('div');
+      el_konami.className = 'pu-Konami';
+      document.body.appendChild(el_konami);
+      
+      // fun stuff here
+      var el_konamiChild = [
+        document.createElement('div'),
+        document.createElement('div')
+      ];
+      
+      el_konamiChild[0].className = 'pu-Konami-content';
+      el_konamiChild[1].className = 'pu-Konami-content-hiddenPart';
+      
+      // add a qr code here  instead of this stupid phrase?
+      el_konamiChild[1].appendChild(document.createTextNode("We've got you !"));
+      
+      el_konamiChild[0].appendChild(el_konamiChild[1]);
+      
+      el_konami.appendChild(el_konamiChild[0]);
+    }
+    
+    // esc key => cancel: remove dom element
+    if (kkeys.toString().indexOf(konami_cancel) >= 0 && el_konami) {
+      document.body.removeChild(el_konami);
+      delete el_konami;
+      el_konami = null;
+    }
+  }, true);
+}
--- src/assets/_scripts/putaindecode.js
+++ src/assets/_scripts/putaindecode.js
@@ -1,1 +1,3 @@
-console.log("bon j'avoue, @comenkoncode c'est nous.");
+if (console && console.log) {
+  console.log("Coucou, pour le code source c'est par là https://github.com/putaindecode/website - Bisous");
+}
