1891f717fb3da8aefd9003963e2205be7daa6d37
Maxime Thirouin <m@moox.io>
2013-11-08 00:11:10 +0100

    COMMIT 200

--- src/assets/_scripts/konami.js
+++ src/assets/_scripts/konami.js
@@ -5,32 +5,40 @@ if (window.addEventListener) {
     konami_cancel = "27"; // esc
     el_konami = null;
   window.addEventListener("keydown", function(e) {
-    kkeys.push(e.keyCode); 
+    kkeys.push(e.keyCode);
     if (kkeys.toString().indexOf(konami) >= 0) {
       // reset
       kkeys = [];
-      
+
       el_konami = document.createElement('div');
       el_konami.className = 'putainde-Konami';
       document.body.appendChild(el_konami);
-      
+
       // fun stuff here
       var el_konamiChild = [
         document.createElement('div'),
-        document.createElement('div')
+        document.createElement('pre')
       ];
-      
+
       el_konamiChild[0].className = 'putainde-Konami-content';
       el_konamiChild[1].className = 'putainde-Konami-content-hiddenPart';
-      
+
       // add a qr code here  instead of this stupid phrase?
-      el_konamiChild[1].appendChild(document.createTextNode("We've got you !"));
-      
+      el_konamiChild[1].appendChild(document.createTextNode(
+' Avoue que tu l\'avez pas vu venir celle là hein ?\n' +
+'__________        __         .__             .___       _________            .___\n' +
+'\\______   \\__ ___/  |______  |__| ____     __| _/____   \\_   ___ \\  ____   __| _/____ \n' +
+' |     ___/  |  \\   __\\__  \\ |  |/    \\   / __ |/ __ \\  /    \\  \\/ /  _ \\ / __ |/ __ \\ \n' +
+' |    |   |  |  /|  |  / __ \\|  |   |  \\ / /_/ \\  ___/  \\     \\___(  <_> ) /_/ \\  ___/ \n' +
+' |____|   |____/ |__| (____  /__|___|  / \\____ |\\___  >  \\______  /\\____/\\____ |\\___  > \n' +
+'                           \\/        \\/       \\/    \\/          \\/            \\/    \\/\n'
+      ));
+
       el_konamiChild[0].appendChild(el_konamiChild[1]);
-      
+
       el_konami.appendChild(el_konamiChild[0]);
     }
-    
+
     // esc key => cancel: remove dom element
     if (kkeys.toString().indexOf(konami_cancel) >= 0 && el_konami) {
       document.body.removeChild(el_konami);
