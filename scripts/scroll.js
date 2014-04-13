cf6091b93cdf18ada7bc73c24c1d011aa5fcbc21
Matthias Le Brun <mlbli@me.com>
2014-04-13 22:04:14 +0200

    footer

---
+++ scripts/scroll.js
@@ -0,0 +1,35 @@
+var cornea = require("cornea")
+  , scroll = require("bloody-scroll")
+
+module.exports = cornea.extend({
+  element : document.body,
+  initialize : function(){
+    this.scrollTo()
+  },
+  events : [
+    {
+      type : "click",
+      selector : ".js-ScrollTo",
+      listener : "scrollTo"
+    }
+  ],
+  scrollTo : function(eventObject, target){
+    var hash = window.location.hash
+      , element
+    if(target) {
+      hash = target.hash
+      eventObject.preventDefault()
+    }
+    if(!hash || hash == "#") {
+      return
+    }
+    element = document.getElementById(hash.slice(1))
+    if(!element) {
+      return
+    }
+    setTimeout(function(){
+      var clientRect = element.getBoundingClientRect()
+      scroll(clientRect.top + window.pageYOffset, 500)
+    }, 300)
+  }
+})
