2b7c4fafb565ff1b788166e24f4b64a57e8fe64f
Matthias Le Brun <mlbli@me.com>
2014-04-13 12:23:50 +0200

    wow post pages with filters

---
+++ scripts/column.js
@@ -0,0 +1,20 @@
+var cornea = require("cornea")
+  , tags = require("./tags")
+
+module.exports = cornea.extend({
+  element : ".js-Column",
+  events : [
+    {
+      type : "change",
+      selector : ".js-ToggleTag",
+      listener : "updateTags"
+    }
+  ],
+  updateTags : function(eventObject, target){
+    if(target.checked) {
+      tags.set(target.value, true)
+      return
+    }
+    tags.remove(target.value)
+  }
+})
