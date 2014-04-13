f68c52040c47dcfbaaa1c099db6fb8b7f9f10afc
Matthias Le Brun <mlbli@me.com>
2014-04-14 00:50:02 +0200

    udpates + jshint

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -1,11 +1,14 @@
 var gulp = require("gulp")
   , plumber = require("gulp-plumber")
   , browserify = require("gulp-browserify")
+  , jshint = require("gulp-jshint")
   , paths = require("./paths")
 
 module.exports = function(){
   return gulp.src(paths.sources.mainScript)
     .pipe(plumber())
+    .pipe(jshint())
+    .pipe(jshint.reporter("default"))
     .pipe(browserify())
     .pipe(gulp.dest(paths.dist.scripts))
 }
