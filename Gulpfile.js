776e89440c454d80358b77635e703b91ac982d40
Maxime Thirouin <m@moox.io>
2014-04-17 05:52:20 +0200

    Clean/reorganize gulpfile a little bit

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,42 +1,49 @@
 var gulp = require("gulp")
-  , paths = require("./tasks/paths")
 
 require("./tasks/fixes/marked")
 
-gulp.task("server", require("./tasks/server").start)
+// html pages
 gulp.task("lang", require("./tasks/lang"))
 gulp.task("exports", require("./tasks/exports"))
 gulp.task("pages", ["lang", "exports"], require("./tasks/pages"))
 gulp.task("feed", ["lang", "exports"], require("./tasks/feed"))
 gulp.task("sitemap", ["lang", "exports"], require("./tasks/sitemap"))
-gulp.task("icons", require("./tasks/icons"))
-gulp.task("fonts", require("./tasks/fonts"))
+
+// static assets
 gulp.task("public", require("./tasks/public"))
 gulp.task("images", require("./tasks/images"))
+gulp.task("icons", require("./tasks/icons"))
+gulp.task("fonts", require("./tasks/fonts"))
+
+// dynamic assets
+gulp.task("scripts", ["scripts:linting"], require("./tasks/scripts"))
+gulp.task("scripts:linting", require("./tasks/scripts-linting"))
 gulp.task("stylesheets", require("./tasks/stylesheets"))
-gulp.task("stylesheets:prod", ["icons"], require("./tasks/stylesheets"))
-gulp.task("scripts", ["linting"], require("./tasks/scripts"))
-gulp.task("linting", require("./tasks/linting"))
+gulp.task("stylesheets:all", ["icons"], require("./tasks/stylesheets")) // for first run, to ensure icon css is fresh & ready
 
-gulp.task("watch", require("./tasks/watch"))
-gulp.task("publish", ["dist"], require("./tasks/publish"))
-
-// aliases
-
+// build
 gulp.task("dist", [
+  "pages",
+  "feed",
+  "sitemap",
+
   "public",
   "images",
   "icons",
   "fonts",
-  "pages",
-  "feed",
-  "sitemap",
+
   "scripts",
-  "stylesheets:prod"
+  "stylesheets:all"
 ])
 
+// dev tasks
+gulp.task("server", require("./tasks/server").start)
+gulp.task("watch", require("./tasks/watch"))
 gulp.task("default", [
   "dist",
   "server",
   "watch"
 ])
+
+// publish
+gulp.task("publish", ["dist"], require("./tasks/publish"))
