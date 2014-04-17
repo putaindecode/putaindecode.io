2e358cd90a8d48cd32031133fbf5b848fd4ed726
Maxime Thirouin <m@moox.io>
2014-04-17 06:08:13 +0200

    Faster script uglify (400ms -> 250ms)

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -1,15 +1,15 @@
 var gulp = require("gulp")
   , plumber = require("gulp-plumber")
   , browserify = require("gulp-browserify")
-  , uglify = require("gulp-uglify")
   , paths = require("./paths")
   , server = require("./server")
 
 module.exports = function(){
   return gulp.src(paths.sources.mainScript)
     .pipe(plumber())
-    .pipe(browserify())
-    .pipe(uglify())
+    .pipe(browserify(
+      {transform : ["uglifyify"]}
+    ))
     .pipe(gulp.dest(paths.dist.scripts))
     .pipe(server.livereload())
 }
