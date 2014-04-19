ffda5734de434c6c161950800a8548e83ee1af6e
Matthias Le Brun <mlbli@me.com>
2014-04-19 12:33:18 +0200

    gulpfile inline docs

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,23 +2,98 @@ var gulp = require("gulp")
 
 require("./tasks/fixes/marked")
 
-// html pages
+/**
+ * task lang
+ *
+ * reads the lang/index.json and stores it in `tasks/cache/lang`
+ */
 gulp.task("lang", require("./tasks/lang"))
+
+/**
+ * task exports
+ *
+ * reads all pages' exports blocks and make and stores them
+ * in `tasks/cache/exports`
+ */
 gulp.task("exports", require("./tasks/exports"))
+
+/**
+ * task pages
+ *
+ * compiles jade to html with `exports`, `lang` and `tasks/cache/options`
+ * available
+ */
 gulp.task("pages", ["lang", "exports"], require("./tasks/pages"))
+
+/**
+ * task feed
+ *
+ * creates xml feed
+ */
 gulp.task("feed", ["pages", "lang", "exports"], require("./tasks/feed"))
+
+/**
+ * task sitemap
+ *
+ * creates an xml sitemap
+ */
 gulp.task("sitemap", ["lang", "exports"], require("./tasks/sitemap"))
 
-// static assets
+/**
+ * task public
+ *
+ * copies `public` contents in `dist`
+ */
 gulp.task("public", require("./tasks/public"))
+
+/**
+ * task images
+ *
+ * copies `images` contents in `dist/images`
+ */
 gulp.task("images", require("./tasks/images"))
+
+/**
+ * task icons
+ *
+ * creates iconfont from `icons` svgs and generates a
+ * css from `stylesheets/template/icons`
+ */
 gulp.task("icons", require("./tasks/icons"))
+
+/**
+ * task fonts
+ *
+ * copies `fonts` to `dist/fonts`
+ */
 gulp.task("fonts", require("./tasks/fonts"))
 
-// dynamic assets
+/**
+ * task scripts
+ *
+ * creates a browserify bundle from `scripts/index`
+ */
 gulp.task("scripts", ["scripts:linting"], require("./tasks/scripts"))
+
+/**
+ * task scripts:linting
+ *
+ * jshint + jscs
+ */
 gulp.task("scripts:linting", require("./tasks/scripts-linting"))
+
+/**
+ * task stylesheets
+ *
+ * stylus -> rework -> css
+ */
 gulp.task("stylesheets", require("./tasks/stylesheets"))
+
+/**
+ * task stylesheets
+ *
+ * stylesheets after icons css has been generated
+ */
 gulp.task("stylesheets:all", ["icons"], require("./tasks/stylesheets")) // for first run, to ensure icon css is fresh & ready
 
 // build
