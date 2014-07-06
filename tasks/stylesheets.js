ee44e9b0a04f19e0d2b6692e45463abc43c08f9e
Maxime Thirouin <m@moox.io>
2014-07-06 08:16:42 +0200

    Use Myth for CSS (current master) + update css libs / fix build issue
    Close #210

--- tasks/stylesheets.js
+++ tasks/stylesheets.js
@@ -2,52 +2,17 @@ var gulp = require("gulp")
 var opts = require("./options")
 var util = require("gulp-util")
 var plumber = require("gulp-plumber")
-var rework = require("gulp-rework")
-var reworkPlugins = {
-  importer : require("rework-importer"),
-  //  parent : require("rework-parent"),
-  //  breakpoints : require("rework-breakpoints"),
-  vars : require("rework-vars"),
-  calc : require("rework-calc"),
-  // colorFn : require("rework-color-function"), // Tab Atkins's proposal color function in CSS
-  // hexAlpha : require("rework-hex-alpha"), // use 4-digit or 8-digit hex colors with alpha channels
-  // inline : require("rework-plugin-inline"),
-  ieLimits : require("rework-ie-limits"),
-  remFallback : require("rework-rem-fallback"),
-  // clearfix : require("rework-clearfix"),
-}
-var autoprefixer = require("gulp-autoprefixer")
+var myth = require("gulp-myth")
 var paths = require("./paths")
 
 /**
  * task stylesheets
  *
- * rework -> css
+ * myth -> css
  */
 module.exports = function(){
   return gulp.src(paths.sources.stylesheets + "/index.css")
     .pipe(opts.plumber ? plumber() : util.noop())
-    .pipe(rework(
-      reworkPlugins.importer({
-        path : paths.sources.stylesheets + "/index.css",
-        base : "."
-      }),
-      rework.colors(),
-      rework.references(),
-      // reworkPlugins.parent,
-      // reworkPlugins.breakpoints,
-      reworkPlugins.vars(),
-      reworkPlugins.calc,
-      // reworkPlugins.colorFn,
-      // reworkPlugins.hexAlpha,
-      // reworkPlugins.inline,
-      reworkPlugins.ieLimits,
-      reworkPlugins.remFallback,
-      // reworkPlugins.clearfix,
-      rework.ease()
-      // rework.extend(),
-      // {sourcemap : !opts.minify}
-    ))
-    .pipe(autoprefixer())
+    .pipe(myth())
     .pipe(gulp.dest(paths.dist.stylesheets))
 }
