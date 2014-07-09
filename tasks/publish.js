448ea267b3433073770c7ed78465d8c7a9620d42
Maxime Thirouin <m@moox.io>
2014-07-09 07:19:56 +0200

    Workaround to be able to publish
    Ref #229

--- tasks/publish.js
+++ tasks/publish.js
@@ -8,7 +8,8 @@ var paths = require("./paths")
  * publish build in the gh-pages branch
  */
 module.exports = function(){
-  return gulp.src(paths.dist.public + "**/*")
+  // https://github.com/rowoot/gulp-gh-pages/issues/26
+  return gulp.src([paths.dist.public + "**/*.*", paths.dist.public + "CNAME"])
     .pipe(ghPages({
       remoteUrl : "https://" + (process.env.GH_TOKEN ? process.env.GH_TOKEN + "@" : "") + "github.com/putaindecode/website.git",
       branch : "gh-pages",
