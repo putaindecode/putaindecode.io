025c59c022a889371f21a2f067e3b4789e3b0ccd
Maxime Thirouin <m@moox.io>
2014-10-13 17:23:09 +0200

    cssnext > myth

--- tasks/stylesheets.js
+++ tasks/stylesheets.js
@@ -2,17 +2,17 @@ var gulp = require("gulp")
 var opts = require("./options")
 var util = require("gulp-util")
 var plumber = require("gulp-plumber")
-var myth = require("gulp-myth")
+var cssnext = require("gulp-cssnext")
 var paths = require("./paths")
 
 /**
  * task stylesheets
  *
- * myth -> css
+ * cssnext -> css
  */
 module.exports = function(){
   return gulp.src(paths.sources.stylesheets + "/index.css")
     .pipe(opts.plumber ? plumber() : util.noop())
-    .pipe(myth({sourcemap : opts.debug}))
+    .pipe(cssnext({sourcemap : opts.debug}))
     .pipe(gulp.dest(paths.dist.stylesheets))
 }
