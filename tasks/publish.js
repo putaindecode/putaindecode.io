8e566bcecf52e80f250a00df8feb0e9f8b90a60b
Maxime Thirouin <m@moox.io>
2014-10-16 07:51:39 +0200

    Close #229 (cannot reproduce)

--- tasks/publish.js
+++ tasks/publish.js
@@ -8,8 +8,7 @@ var paths = require("./paths")
  * publish build in the gh-pages branch
  */
 module.exports = function(){
-  // https://github.com/rowoot/gulp-gh-pages/issues/26
-  return gulp.src([paths.dist.public + "**/*.*", paths.dist.public + "CNAME"])
+  return gulp.src([paths.dist.public + "**/*"])
     .pipe(ghPages({
       remoteUrl : "https://" + (process.env.GH_TOKEN ? process.env.GH_TOKEN + "@" : "") + "github.com/putaindecode/website.git",
       branch : "gh-pages",
