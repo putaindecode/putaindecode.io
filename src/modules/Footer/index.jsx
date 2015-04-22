5e57c19b0e09ae4b9d1b4918f1ca12f1c0e1e2cf
Maxime Thirouin <m@moox.io>
2015-04-22 07:23:29 +0200

    Add footer

---
+++ src/modules/Footer/index.jsx
@@ -0,0 +1,66 @@
+import React, {Component, PropTypes} from "react"
+
+import Icon from "../Icon"
+
+export default class Footer extends Component {
+
+  static displayName = "Footer"
+
+  static contextTypes = {
+    i18n: PropTypes.object,
+  }
+
+  render() {
+    const i18n = this.context.i18n
+    return (
+      <div className="putainde-Footer">
+        <div className="r-Grid">
+
+          <div className="r-Grid-cell r-all--3of12">
+            <p className="putainde-Footer-text">
+              {i18n.copyright.replace("{*}", new Date().getFullYear())}
+            </p>
+          </div>
+
+          <div className="r-Grid-cell r-all--6of12">
+            {
+              i18n.footerNavigation &&
+              <ul className="putainde-Footer-list">
+              {
+                [
+                  i18n.footerNavigation.map((item) => {
+                    return (
+                      <li>
+                        <a href={item.url}>
+                          {item.name}
+                        </a>
+                      </li>
+                    )
+                  }),
+                  <li>
+                    <a href={i18n.github} data-r-tooltip="GitHub" className="r-Tooltip r-Tooltip--top">
+                      <Icon src="/icons/github.svg" />
+                    </a>
+                  </li>,
+                  <li>
+                    <a href={i18n.twitter} data-r-tooltip="Twitter" className="r-Tooltip r-Tooltip--top">
+                      <Icon src="/icons/twitter.svg" />
+                    </a>
+                  </li>,
+                ]
+              }
+              </ul>
+            }
+          </div>
+
+          <div className="r-Grid-cell r-all--3of12">
+            <p className="putainde-Footer-text putainde-Footer-text--small putainde-Footer-text--right">
+              {i18n.madeWithHeart}
+            </p>
+          </div>
+
+        </div>
+      </div>
+    )
+  }
+}
