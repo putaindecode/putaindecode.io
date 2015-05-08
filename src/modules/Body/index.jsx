c35c9fdc370639fa59c2f5c7ac7e2a28492e63ab
Maxime Thirouin <m@moox.io>
2015-05-08 13:38:07 +0200

    some lint fixes

--- src/modules/Body/index.jsx
+++ src/modules/Body/index.jsx
@@ -18,7 +18,7 @@ export default class Body extends Component {
   static propTypes = {
     children: PropTypes.oneOfType([
       PropTypes.array,
-      PropTypes.object
+      PropTypes.object,
     ]).isRequired,
   }
 
