4f18fbe18a6f61a4ef80d6434e84ce12ba863a20
Guillaume Demesy <guillaume.demesy@gmail.com>
2015-11-03 21:24:28 +0100

    Added svgo-loader for cleaning SVG raw
    Close #507

--- scripts/build.js
+++ scripts/build.js
@@ -47,7 +47,13 @@ const webpackConfig = {
   resolveLoader: {
     root: [ path.join(root, "node_modules") ],
   },
-
+  svg : {
+      plugins: [
+        {removeTitle: true, removeDesc: true},
+        {convertColors: {shorthex: false}},
+        {convertPathData: false},
+      ],
+  },
   module: {
     // ! \\ note that loaders are executed from bottom to top !
     loaders: [
@@ -115,7 +121,10 @@ const webpackConfig = {
       },
       {
         test: /\.(svg)$/,
-        loader: "raw-loader",
+        loaders : [
+          "raw",
+          "svgo?useConfig=svg",
+        ],
       },
     ],
   },
