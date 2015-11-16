d1df5251d4edfcb2c81ed2747261ffe08b9d1819
Maxime Thirouin <m@moox.io>
2015-11-16 07:34:30 +0100

    Create a Contributor component (avatar + name/commits in tooltip)

---
+++ web_modules/Contributor/index.js
@@ -0,0 +1,44 @@
+import React, { Component, PropTypes } from "react"
+import cx from "classnames"
+
+import Avatar from "../Avatar"
+
+export default class Contributors extends Component {
+
+  static propTypes = {
+    author: PropTypes.string.isRequired,
+    commits: PropTypes.string,
+    size: PropTypes.string,
+  }
+
+  render() {
+    const {
+      author,
+      commits,
+      size,
+    } = this.props
+
+    return (
+      <div
+        key={ author }
+        className={ cx(
+          "putainde-Contributor",
+          "r-Tooltip",
+          "r-Tooltip--bottom",
+          "r-Tooltip--allowNewLines"
+        ) }
+        data-r-tooltip={ [
+          author,
+          `(${ commits } commit${ commits > 1 ? "s" : "" })`,
+        ].join("\n") }
+      >
+        <Avatar
+          author={ author }
+          className={
+            `putainde-Contributor-avatar${ size ? `--${ size }` : "" }`
+          }
+        />
+      </div>
+    )
+  }
+}
