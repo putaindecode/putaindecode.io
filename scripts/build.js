cb0a4bd0327b904db2216023ff31672c2ae59ca2
Maxime Thirouin <m@moox.io>
2015-11-21 07:56:12 +0100

    Enhance banner style a little
    https://github.com/putaindecode/putaindecode.io/pull/536#issuecomment-15
    8597881

--- scripts/build.js
+++ scripts/build.js
@@ -156,7 +156,22 @@ const webpackConfig = {
 
   postcss: (webpack) => [
     require("postcss-import")({ addDependencyTo: webpack }),
-    require("postcss-cssnext"),
+    require("postcss-cssnext")({
+      features: {
+        customMedia: {
+          extensions: {
+            maxS: "(max-width: 30em)",
+            minS: "(min-width: 30.01em)",
+            maxM: "(max-width: 50em)",
+            minM: "(min-width: 50.01em)",
+            maxL: "(max-width: 65em)",
+            minL: "(min-width: 65.01em)",
+            maxXL: "(max-width: 80em)",
+            minXL: "(min-width: 80.01em)",
+          },
+        },
+      },
+    }),
   ],
 
   plugins: [
