372b673d4290a3a5038290abec060944f691102e
Maxime Thirouin <m@moox.io>
2015-11-23 07:52:43 +0100

    Fix og:site_name and add lang attribute on the content

--- web_modules/Layout/index.js
+++ web_modules/Layout/index.js
@@ -2,6 +2,8 @@ import React, { Component } from "react"
 import { PropTypes } from "react"
 import Helmet from "react-helmet"
 
+import getLang from "i18n/getLang"
+import getI18n from "i18n/get"
 import Header from "Header"
 import Footer from "Footer"
 import GoogleAnalyticsTracker from "GoogleAnalyticsTracker"
@@ -15,20 +17,19 @@ export default class Layout extends Component {
 
   static contextTypes = {
     metadata: PropTypes.object.isRequired,
+    location: PropTypes.object.isRequired,
   }
 
   render() {
-    const {
-      pkg,
-      i18n,
-    } = this.context.metadata
+    const i18n = getI18n(this.context)
+    const locale = getLang(this.context)
 
     return (
-      <div className="r-VerticalRhythm">
+      <div className="r-VerticalRhythm" lang={ locale }>
         <GoogleAnalyticsTracker params={ this.props.params }>
           <Helmet
             meta={[
-              { property: "og:site_name", content: pkg.name },
+              { property: "og:site_name", content: i18n.title },
               { name: "twitter:site", content: `@${ i18n.twitterUsername }` },
             ]}
           />
